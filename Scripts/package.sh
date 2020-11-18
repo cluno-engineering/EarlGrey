d838678b9cb7527321292d1be90f92b74956ee49
Chris Dite <chris.dite@cluno.com>
2020-11-18 17:15:21 +0100

    Package name

--- Scripts/package.sh
+++ Scripts/package.sh
@@ -24,7 +24,7 @@ CURRENT_DIR="${PWD}"
 : "${PROJECT_FILE_PATH:=${SOURCE_DIR}/../EarlGrey.xcodeproj}"
 : "${OUTPUT_DIR:=${SOURCE_DIR}/build}"
 
-readonly PACKAGE_NAME="EarlGrey"
+readonly PACKAGE_NAME="ClunoEarlGrey"
 readonly OUTPUT_TMP_DIR=$(mktemp -d)
 readonly OUTPUT_PACKAGE_DIR="${OUTPUT_TMP_DIR}/${PACKAGE_NAME}"
 readonly PACKAGE_FILES="README.md CHANGELOG.md LICENSE"
@@ -39,7 +39,7 @@ mkdir -p "${OUTPUT_PACKAGE_DIR}"
 (cd "${SOURCE_DIR}/.." && cp "${PACKAGE_FILES}" "${OUTPUT_PACKAGE_DIR}")
 
 # Symlink the framework so we don't need to copy.
-ln -s "${OUTPUT_DIR}/EarlGrey.framework" "${OUTPUT_PACKAGE_DIR}/EarlGrey.framework"
+ln -s "${OUTPUT_DIR}/ClunoEarlGrey.framework" "${OUTPUT_PACKAGE_DIR}/ClunoEarlGrey.framework"
 
 (cd "${OUTPUT_DIR}/.." \
   && zip -r -X "${CURRENT_DIR}/${PACKAGE_NAME}.zip" "${PACKAGE_NAME}")
